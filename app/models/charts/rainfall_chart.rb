8518fbdc2696fb201db04e7116c74d4f8fd4af02
Matthew Hollingworth <mdholling@gmail.com>
2009-10-03 11:46:50 +1000

    Small changes to Rainfall chart data presentation.

--- app/models/charts/rainfall_chart.rb
+++ app/models/charts/rainfall_chart.rb
@@ -1,16 +1,15 @@
 class RainfallChart < Chart
   def data(interval)
     observations = instrument.observations.chronological.during(interval)
-    series = observations.inject([ 0.0, [ ] ]) do |(sum, points), observation|
-      sum += observation.value
-      [ sum, points << [ observation.time.to_js, sum ] ]
-    end.last
-    
-    if observations.first
-      series.unshift [ [ observations.first.time - instrument.default_interval, interval.begin ].max.to_js, 0.0 ]
+    finish = [ interval.end, ActiveSupport::TimeWithZone.now ].min
+    series = if observations.any?
+      observations.inject([ 0.0, [ [ interval.begin.to_js, 0.0 ] ] ]) do |(sum, points), observation|
+        sum += observation.value
+        [ sum, points << [ observation.time.to_js, sum ] ]
+      end.last << [ finish.to_js, observations.sum(:value) ]
+    else
+      [ [ interval.begin.to_js, 0.0 ], [ finish.to_js, 0.0 ] ]
     end
-    series.unshift [ interval.begin.to_js, 0.0 ]
-    series.push    [ [ interval.end, ActiveSupport::TimeWithZone.now ].min.to_js, series.last[1] ]
     [ series ]
   end
 end
