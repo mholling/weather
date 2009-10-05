4f453d4e80f6fe4b255279b9b41c7ceae933778b
Matthew Hollingworth <mdholling@gmail.com>
2009-10-05 15:11:24 +1100

    Fixed calendar date range

--- app/controllers/observation_ranges_controller.rb
+++ app/controllers/observation_ranges_controller.rb
@@ -3,7 +3,7 @@ class ObservationRangesController < ApplicationController
     first = Observation.chronological.first.time
     last = Observation.chronological.last.time
     respond_to do |format|
-      format.json { render :json => { "minDate" => first.to_js, "maxDate" => last.to_js } }
+      format.json { render :json => { "minDate" => first.to_i*1000, "maxDate" => last.to_i*1000 } }
     end
   end
 end
