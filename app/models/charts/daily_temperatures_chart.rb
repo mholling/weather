0071204d07597002b2b85078248d6a2d6602da27
Matthew Hollingworth <mdholling@gmail.com>
2009-10-06 18:04:13 +1100

    Improvements to scalings.js. Swapped flot 1.5 for flot SVN.

--- app/models/charts/daily_temperatures_chart.rb
+++ app/models/charts/daily_temperatures_chart.rb
@@ -6,10 +6,9 @@ class DailyTemperaturesChart < Chart
     finish = meteorological_day_including(interval.end).end
     maximums = instrument.observations.with_value.during(start..finish).maximum(:value, :group => :meteorological_date)
     minimums = instrument.observations.with_value.during(start..finish).minimum(:value, :group => :meteorological_date)
-    # TODO: need to use non_nil filter once we let in nil values for observations! (in daily rainfall also!)
-    maximum_series = [ maximums.keys.map(&:beginning_of_day).map(&:to_js), maximums.values ].transpose
-    minimum_series = [ minimums.keys.map(&:beginning_of_day).map(&:to_js), minimums.values ].transpose
-    [ maximum_series, minimum_series ]
+    series = [ maximums.keys.map(&:beginning_of_day).map(&:to_js), maximums.values, minimums.values ].transpose
+    [ series ]
+
   end
 
 end
