1056da238b7c5f340bf948be504892155e2ca25a
Matthew Hollingworth <mdholling@gmail.com>
2009-10-09 07:53:25 +1100

    stylesheet changes

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,7 +2,6 @@ class StatisticsController < ApplicationController
   def show
     @scaling = Scaling.for(Statistic).find(params[:scaling_id])
     date = Date.parse(params[:date])
-    sleep(2)
     respond_to do |format|
       format.json { render :json => { :message => @scaling.statistic.name } }
     end
