90f77b16259ffa5abb047775792816623af19bf1
Matthew Hollingworth <mdholling@gmail.com>
2009-10-08 23:07:57 +1100

    consolidated scalings.js

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,6 +1,6 @@
 class ChartsController < ApplicationController
   def show
-    @scaling = Scaling.find(params[:scaling_id])
+    @scaling = Scaling.for(Chart).find(params[:scaling_id])
     date = Date.parse(params[:date])
     respond_to do |format|
       format.json { render :json => { "data" => @scaling.data(date), "options" => @scaling.options(date) } }
--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,2 +1,10 @@
 class StatisticsController < ApplicationController
+  def show
+    @scaling = Scaling.for(Statistic).find(params[:scaling_id])
+    date = Date.parse(params[:date])
+    sleep(2)
+    respond_to do |format|
+      format.json { render :json => { :message => @scaling.statistic.name } }
+    end
+  end
 end
