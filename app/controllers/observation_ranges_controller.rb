3a1e34fbf0b09d4c8886998edd1cf174fffd4af9
Matthew Hollingworth <mdholling@gmail.com>
2009-10-10 20:46:52 +1100

    revamped scales and made each scale have its own date picker

--- app/controllers/observation_ranges_controller.rb
+++
@@ -1,9 +0,0 @@
-class ObservationRangesController < ApplicationController
-  def show
-    first = Observation.chronological.first.time
-    last = Observation.chronological.last.time
-    respond_to do |format|
-      format.json { render :json => { "minDate" => first.to_i*1000, "maxDate" => last.to_i*1000 } }
-    end
-  end
-end
